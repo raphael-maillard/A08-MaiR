ad07b81fc6b7fa090908a6534bc1ebd9b5b525d3
RaphaelM <raphael.maillard@gmail.com>
2020-07-31 17:31:45 +0200

    Update db.php and git.ignore

--- settings/db.php
+++ settings/db.php
@@ -1,11 +1,13 @@
 <?php
 
-$host = "localhost";
-$dbname = "a08-hern"; // to adapt
-$login = "root";
-$mdp = "";
+$database = 'mysql:host=127.0.0.1:3308; dbname=a08-MaiR;';
+$user = 'a08';
+$pwd = 'mdp';
 
-/**
- * try connection
- * catch and display the error and stop process
- */
+try {
+    $connect = new PDO($database, $user, $pwd);
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // print_r ("Connexion réussi <br>");    
+} catch (PDOException $e) {
+    echo 'Connexion échouée : ' . $e->getMessage();
+}
