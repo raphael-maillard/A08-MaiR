87be55a4aca0dff4b6270a7552c58e922f70c900
RaphaelM <raphael.maillard@gmail.com>
2020-07-31 15:28:08 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php include_once './settings/db.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">c 
+
+    <?php include_once './templates/head.html'; ?>
+
+    <body>
+
+        <?php include_once './templates/header.html'; ?>
+
+        <?php include_once './templates/nav.html'; ?>
+
+        <section>
+            <?php
+
+            /**
+             * Conditions with:
+             *      - if/else
+             *          or
+             *      - switch/case
+             */
+
+            /** 
+             * if ...
+             *      include_once './templates/movies/_form_new.php';
+             * 
+             * elseif ...
+             *      include_once './templates/movies/_form_edit.php';
+             * 
+             * elseif ...
+             *      ...
+             * 
+             * ...
+             * 
+             * else ...
+             *      - welcome message
+             *      - button for add new movie
+             */
+
+            ?>
+        </section>
+
+        <?php include_once './templates/footer.html'; ?>
+
+    </body>
+
+</html>
---
+++ settings/db.php
@@ -0,0 +1,11 @@
+<?php
+
+$host = "localhost";
+$dbname = "a08-hern"; // to adapt
+$login = "root";
+$mdp = "";
+
+/**
+ * try connection
+ * catch and display the error and stop process
+ */
