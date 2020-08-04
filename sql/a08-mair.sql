17c48929bbe923aa59b26ab017185f6d7b3fe8ec
RaphaelM <raphael.maillard@gmail.com>
2020-08-04 10:49:30 +0200

    Update save DBB

--- sql/a08-mair.sql
+++ sql/a08-mair.sql
@@ -2,10 +2,10 @@
 -- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3308
--- Generation Time: Aug 02, 2020 at 08:24 PM
--- Server version: 5.7.28
--- PHP Version: 7.3.12
+-- Hôte : 127.0.0.1:3308
+-- Généré le :  mar. 04 août 2020 à 08:48
+-- Version du serveur :  5.7.28
+-- Version de PHP :  7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,37 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `a08-mair`
+-- Base de données :  `a08-mair`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `images`
---
-
-DROP TABLE IF EXISTS `images`;
-CREATE TABLE IF NOT EXISTS `images` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `image` varchar(255) CHARACTER SET utf8 NOT NULL,
-  `alt` varchar(255) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `images`
---
-
-INSERT INTO `images` (`id`, `image`, `alt`) VALUES
-(1, 'incredible-hulk.jpg', 'Affiche du film de L\'incroyble Hulk'),
-(2, 'gardiens.jpg', 'Affiche film gardiens de la galaxie'),
-(3, 'ironman.jpg', 'Affiche film ironman'),
-(4, 'thor_le_monde_des_tenebres', 'Affiche du Film Thor le monde des ténébres');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `movies`
+-- Structure de la table `movies`
 --
 
 DROP TABLE IF EXISTS `movies`;
@@ -59,30 +35,29 @@ CREATE TABLE IF NOT EXISTS `movies` (
   `release_date` date NOT NULL,
   `duration` time DEFAULT NULL,
   `director` varchar(80) CHARACTER SET utf8 DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `id_phase` tinyint(3) DEFAULT NULL,
-  `id_image` int(11) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `modified_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`,`release_date`),
-  KEY `movies_id_images` (`id_image`),
   KEY `movies_id_phases` (`id_phase`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `movies`
+-- Déchargement des données de la table `movies`
 --
 
-INSERT INTO `movies` (`id`, `name`, `release_date`, `duration`, `director`, `id_phase`, `id_image`, `created_at`, `modified_at`) VALUES
-(1, 'IronMan', '2008-04-30', '02:06:00', 'Jon Favreau', 1, 3, '2020-07-31 17:39:06', NULL),
-(2, 'Les Gardiens de la Galaxie', '2014-08-07', '02:01:00', 'James Gunn', 1, 2, '2020-07-31 17:40:32', NULL),
-(3, 'L\'incroyable Hulk', '2008-07-23', '01:52:00', 'Louis Leterrier', 1, 1, '2020-07-31 17:50:16', NULL),
-(4, 'Thor le Monde des ténébres', '2013-10-30', '01:52:00', 'Alan Taylor', 2, 4, '2020-08-01 12:06:07', NULL);
+INSERT INTO `movies` (`id`, `name`, `release_date`, `duration`, `director`, `image`, `id_phase`, `created_at`, `modified_at`) VALUES
+(1, 'IronMan', '2008-04-30', '02:06:00', 'Jon Favreau', 'ironman.jpg', 1, '2020-07-31 17:39:06', NULL),
+(2, 'Les Gardiens de la Galaxie', '2014-08-07', '02:01:00', 'James Gunn', 'gardiens.jpg', 1, '2020-07-31 17:40:32', NULL),
+(3, 'L\'incroyable Hulk', '2008-07-23', '01:52:00', 'Louis Leterrier', 'incredible-hulk.jpg', 1, '2020-07-31 17:50:16', NULL),
+(4, 'Thor le Monde des ténébres', '2013-10-30', '01:52:00', 'Alan Taylor', 'thor_le_monde_des_tenebres.jpg', 2, '2020-08-01 12:06:07', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `phases`
+-- Structure de la table `phases`
 --
 
 DROP TABLE IF EXISTS `phases`;
@@ -93,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `phases` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `phases`
+-- Déchargement des données de la table `phases`
 --
 
 INSERT INTO `phases` (`id`, `phase`) VALUES
@@ -102,14 +77,13 @@ INSERT INTO `phases` (`id`, `phase`) VALUES
 (3, 'III');
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `movies`
+-- Contraintes pour la table `movies`
 --
 ALTER TABLE `movies`
-  ADD CONSTRAINT `movies_id_images` FOREIGN KEY (`id_image`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `movies_id_phases` FOREIGN KEY (`id_phase`) REFERENCES `phases` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
